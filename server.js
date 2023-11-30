b3c48df3c3aa48554ccc4b768181604cb090eb2d
abkhan505 <khanaban505@gmail.com>
2023-11-30 13:39:08 -0500

    Added sendFile for CSS files

--- server.js
+++ server.js
@@ -998,6 +998,9 @@ let constructs = {
 // GET request (Read request) that looks for the URL requesting the index.html file, responds with it
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html') // Responding to the request by sending our index.html file. __dirname states that wherever server.js file is located, that is where we will start looking four our files.
+    response.sendFile(__dirname + '/css/style.css')
+    response.sendFile(__dirname + '/css/reset.css')
+    response.sendFile(__dirname + '/css/normalize.css')
 })
 
 app.get('/api', (request, response) => { // Responding to API request by sending JSON. '/:name' is part of the URL so someone can replcae this with a specific object name and receive just that back. ':' reminds us that it is part of the query parameter. It is not part of the path.
